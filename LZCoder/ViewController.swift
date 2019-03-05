0c5e783a666af35a4709117ecc69ee75523dec09
Kehlin <kehlinswain@gmail.com>
2019-03-04 18:27:25 -0600

    Initial Commit

---
+++ LZCoder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LZCoder
+//
+//  Created by kehlin swain on 3/4/19.
+//  Copyright © 2019 kehlin swain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
