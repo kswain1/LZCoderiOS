f6ae83f890c7a08482f218b3a25ead0056b5e83b
Kehlin <kehlinswain@gmail.com>
2019-09-26 18:08:05 -0500

    request received authorized

--- LZCoder/Network/CodeNetwork.swift
+++ LZCoder/Network/CodeNetwork.swift
@@ -11,10 +11,61 @@ import Alamofire
 
 class CodeNetwork: BaseNetworking {
     
+    
+    func fetchData() {
+        
+       // let request = URLRequest(url: <#T##URL#>)
+        //request.http
+        
+        
+        
+    }
+    
+    
+    
     func getCodesList(params:Parameters , completionHandler: @escaping (_ response: DataResponse<Data>) -> Void ) {
-        request("\(Api.BaseUrl)apiFreeText.json", method: .get, parameters: params, headers: httpHeaders()).responseData { (responseData) in
+    
+        let username = "lzcoder95@gmail.com"
+        let password = "lzcoder123"
+        
+        
+      //  params.forEach { ((key: String, value: Any)) in
+      
+        
+        
+        let url = URL(string: "\(Api.BaseUrl)apiFreeText.json")
+        var urlRequest = URLRequest(url: url!)
+        urlRequest.httpMethod = "POST"
+        urlRequest.addValue("username", forHTTPHeaderField: username)
+        urlRequest.addValue("password", forHTTPHeaderField: password)
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+       // urlRequest.addValue(, forHTTPHeaderField: <#T##String#>)
+        
+        
+       // urlRequest.addValue(, forHTTPHeaderField: <#T##String#>)
+      //  request(<#T##url: URLConvertible##URLConvertible#>, method: <#T##HTTPMethod#>, parameters: <#T##Parameters?#>, encoding: <#T##ParameterEncoding#>, headers: <#T##HTTPHeaders?#>)
+        
+        request(urlRequest).response { (response) in
+            
+            if let error = response.error {
+                print("error fetching data \(error.localizedDescription)")
+            }
+            
+            let str = String(bytes: response.data!, encoding: .utf8)
+            
+            
+            print("response \(str)")
+        }
+        //request.
+        /*
+        request(request
+             ).responseData { (responseData) in
+            guard let data = responseData.data else {return}
+             let string = String(bytes: data, encoding: .utf8)
+            print("data \(string)")
             completionHandler(responseData)
         }
+ */
     }
     
     func getCodesDetails(code:String, completionHandler: @escaping (_ response: DataResponse<Data>) -> Void ) {
